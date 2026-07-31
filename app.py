8fdfe92212c9bb9ad164e0a229ce4f6d29db5f65
sarvagyaj210-ops <sarvagyaj210@gmail.com>
2026-07-31 15:31:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,16 +25,19 @@ ALL_API = [GOOGLE_API_KEY, GROQ_API_KEY, TAVILY_API_KEY]
 if not all(ALL_API):
   st.sidebar.error("PASS API-KEYS")
 
-elif any(ALL_API):
-  st.sidebar.info("Must pass all API-KEYS")
-
-else:
-  st.sidebar.success("API KEYS LOADED SUCCESSFULLY")
+elif all(ALL_API):
   # Step 1: Model call
   model = ChatGoogleGenerativeAI(
-      model = "gemini-3.5-flash-lite",
-      google_api_key = GOOGLE_API_KEY
-  )
+        model = "gemini-3.5-flash-lite",
+        google_api_key = GOOGLE_API_KEY
+    )
+  st.sidebar.success("API KEYS LOADED SUCCESSFULLY")
+
+elif any(ALL_API):
+  st.sidebar.info("Must pass all API-KEYS")
+  
+else:
+  st.info("LOADED")
 
 # =============FRONT END=============
 st.title("AGENT-POWERED PPT GENERATOR")
@@ -93,47 +96,49 @@ def prompt_generator(model, query):
     f.write(final_prompt)
   return final_prompt
 
-agent = create_agent(
-    model = model,
-    tools = [search_latest_info,
-             generate_image
-             ]
-)
+if all(ALL_API) and user_query:
+    agent = create_agent(
+      model = model,
+      tools = [search_latest_info,
+               generate_image
+               ]
+  )
+  
+  # ===================DISPLAY AGENT=================
+  # st.sidebar.image(agent)
+  
+  # ================== WITH TABS =================
+  with tab1:
+    st.header("GENERATE IMAGE GIVE PROMPT")
+    if st.button("CLICK TO GENERATE: ", key = "generate_img_button"):
+      with st.spinner("Running Agent..."):
+        data = f"https://image.pollinations.ai/{user_query}"
+        time.sleep(3)
+        st.image(data)
+        # st.image("Image.jpeg")
+        # st.return()
 
-# ===================DISPLAY AGENT=================
-st.sidebar.image(agent)
-
-# ================== WITH TABS =================
-with tab1:
-  st.header("GENERATE IMAGE GIVE PROMPT")
-  if st.button("CLICK TO GENERATE: ")
-  with st.spinner("Running Agent..."):
-    generate_image(user_query)
-    st.image(data)
-    st.image("Image.jpeg")
-
-with tab2:
-  st.header("CHECK LATEST NEWS")
-  if st.button("Fetch news: "):
-    with st.spinner("Running Agent..."):
-      prompt = """ Give latest news India or world news rated to tech, business, jobs 
-      or user requested Output in proper HTML News Templates""" + user_query
-
-      response = agent.invoke({'messages':[{'role':"user","content":prompt}]})
-      code = response['messages'][-1].content[-1]['text']
-
-      st.html(code, width = "stretch", unsafe_allow_javascript = True)
--
-with tab3:
-  st.header("Create PPT")
-  if st.button("Click to generate: "):
-    with st.spinner("Running Agent..."):
-      final_prompt = prompt_generator(model, user_query)
-
-      response = agent.invoke({'messages':[{'role':"user","content":final_prompt}]})
-
-      code = response['messages'][-1].content[-1]['text']
-      st.html(code, width = "stretch", unsafe_allow_javascript = True)
-      st.download_button(label = "DOWNLOAD PPT", data = code, file_name = 'ppt.html', mime = 'text/html')
-      st.success("PPT Downloaded Successfully!!")
-      
+  with tab2:
+    st.header("CHECK LATEST NEWS")
+    if st.button("Fetch news: ", key = "news_button"):
+      with st.spinner("Running Agent..."):
+        prompt = """ Give latest news India or world news rated to tech, business, jobs 
+        or user requested Output in proper HTML News Templates""" + user_query
+  
+        response = agent.invoke({'messages':[{'role':"user","content":prompt}]})
+        code = response['messages'][-1].content[-1]['text']
+  
+        st.html(code, width = "stretch", unsafe_allow_javascript = True)
+  -
+  with tab3:
+    st.header("Create PPT")
+    if st.button("Click to generate: ", key = "generate_ppt_button"):
+      with st.spinner("Running Agent..."):
+        final_prompt = prompt_generator(model, user_query)
+  
+        response = agent.invoke({'messages':[{'role':"user","content":final_prompt}]})
+  
+        code = response['messages'][-1].content[-1]['text']
+        st.html(code, width = "stretch", unsafe_allow_javascript = True)
+        if st.download_button(label = "DOWNLOAD PPT", data = code, file_name = 'ppt.html', mime = 'text/html'):
+          st.success("PPT Downloaded Successfully!!")
